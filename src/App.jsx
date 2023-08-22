65161d8f120c421c95c9a6ff438512e1ea4600ab
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-22 14:14:40 -0400

    crate the url

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const url = "https://course-api.netlify.app/api/react-tabs-project";
+
+  return <></>;
+}
+
+export default App;
