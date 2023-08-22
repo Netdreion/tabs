6a397c7e6f7ff24e7a3f621ef13894ebc9ccf26e
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-22 14:38:41 -0400

    fetching the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const url = "https://course-api.netlify.app/api/react-tabs-project";
+  const url = "https://course-api.com/react-tabs-project";
 
-  return <></>;
+  return (
+    <>
+      <h2>Tabs Project Setup</h2>
+    </>
+  );
 }
 
 export default App;
